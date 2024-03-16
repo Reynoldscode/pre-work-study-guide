8ab6cb57c065f329de5a2e89c631708e33d96420
Reynolds Addy <reynoldsfrimpong@gmail.com>
2024-03-15 21:06:23 -0500

    I added JavaScript-file to allow the webpage functional

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(let x = 0; x < topics.length; x++ ){
+    console.log(topics[x]);
+}
+
+/*
+
+if (topics === 'HTML') {
+    console.log("lets study HTML!");
+
+} else if (topics === 'CSS') {
+    console.log("Let's study CSS");
+
+} else if (topics === 'Git') {
+    console.log("Let's study Git");
+
+}else if(topis === 'JavaScript') {
+    console.log ("Let's study JavaScript");
+
+} else {
+    console.log('Please try again!')
+}
+*/
