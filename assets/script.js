4f89df495ed438468366861d51177331af3008d5
Reynolds Addy <reynoldsfrimpong@gmail.com>
2024-03-16 19:17:11 -0500

    Added two functions in the JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,38 @@
-const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+const randomtopics = topics[Math.random() *topics.length];
+
+
+
+function listtopics() {
+    const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for(let x = 0; x < topics.length; x++ ){
     console.log(topics[x]);
 }
 
-/*
-
-if (topics === 'HTML') {
-    console.log("lets study HTML!");
-
-} else if (topics === 'CSS') {
-    console.log("Let's study CSS");
-
-} else if (topics === 'Git') {
-    console.log("Let's study Git");
-
-}else if(topis === 'JavaScript') {
-    console.log ("Let's study JavaScript");
-
-} else {
-    console.log('Please try again!')
 }
-*/
+
+function selecttopics() { 
+    if (randomtopics === 'HTML') {
+        console.log("lets study HTML!");
+    
+    } else if (randomtopics === 'CSS') {
+        console.log("Let's study CSS");
+    
+    } else if (randomtopics === 'Git') {
+        console.log("Let's study Git");
+    
+    }else if(randomtopics === 'JavaScript') {
+        console.log ("Let's study JavaScript");
+    
+    } else {
+        console.log('Please try again!')
+    }
+
+}
+
+console.log('Here are the topics we learned throught Prework');
+listtopics();
+console.log('Which topic should we learn first?');
+selecttopics();
+
